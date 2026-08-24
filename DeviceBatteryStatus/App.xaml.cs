c3e94d9dbfb71ae5aea2d57b8805562537e43ef3
Mateusz Chojak <mateusz.chojak@icloud.com>
2026-08-24 22:18:46 +0200

    feat: add charging lightning indicator to battery widget

--- DeviceBatteryStatus/App.xaml.cs
+++ DeviceBatteryStatus/App.xaml.cs
@@ -2,7 +2,7 @@
 using System.Data;
 using System.Windows;
 
-namespace Stan_baterii
+namespace DeviceBatteryStatus
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- DeviceBatteryStatus/MainWindow.xaml.cs
+++ DeviceBatteryStatus/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
 
-namespace Stan_baterii
+namespace DeviceBatteryStatus
 {
     public partial class MainWindow : Window
     {
@@ -50,17 +50,22 @@ namespace Stan_baterii
                 return;
             }
 
-            ProcessModule module = null;
+            ProcessModule guiModule = null;
+            ProcessModule coreModule = null;
+
             foreach (ProcessModule m in process.Modules)
             {
                 if (m.ModuleName.Equals("Qt5Gui.dll", StringComparison.OrdinalIgnoreCase))
                 {
-                    module = m;
-                    break;
+                    guiModule = m;
+                }
+                else if (m.ModuleName.Equals("Qt5Core.dll", StringComparison.OrdinalIgnoreCase))
+                {
+                    coreModule = m;
                 }
             }
 
-            if (module == null)
+            if (guiModule == null)
             {
                 System.Diagnostics.Debug.WriteLine("Nie znaleziono modułu Qt5Gui.dll.");
                 return;
@@ -71,10 +76,8 @@ namespace Stan_baterii
 
             try
             {
-                IntPtr baseAddress = module.BaseAddress + 0x0058D080;
-
+                IntPtr baseAddress = guiModule.BaseAddress + 0x0058D080;
                 int[] offsets = { 0x10, 0x30, 0x90, 0x30, 0x58, 0x28, 0xA0 };
-
                 IntPtr currentAddress = baseAddress;
 
                 for (int i = 0; i < offsets.Length; i++)
@@ -90,11 +93,44 @@ namespace Stan_baterii
                 ReadProcessMemory(processHandle, currentAddress, valueBuffer, valueBuffer.Length, out _);
                 int batteryLevel = BitConverter.ToInt32(valueBuffer, 0);
 
-                if (batteryLevel >= 0 && batteryLevel <= 100)
+                int isCharging = 0;
+                if (coreModule != null)
                 {
-                    BatteryText.Text = $"{batteryLevel}%";
-                    BatteryFill.Width = (batteryLevel / 100.0) * 46.0;
+                    IntPtr chargeAddress = coreModule.BaseAddress + 0x0055DEE8;
+                    int[] chargeOffsets = { 0x2C8, 0x8, 0x60, 0x8, 0x80, 0x28, 0x8 };
+                    IntPtr currentChargeAddress = chargeAddress;
+
+                    for (int i = 0; i < chargeOffsets.Length; i++)
+                    {
+                        byte[] pointerBuffer = new byte[8];
+                        ReadProcessMemory(processHandle, currentChargeAddress, pointerBuffer, pointerBuffer.Length, out _);
+                        long nextAddress = BitConverter.ToInt64(pointerBuffer, 0);
+
+                        currentChargeAddress = new IntPtr(nextAddress + chargeOffsets[i]);
+                    }
+
+                    byte[] chargeBuffer = new byte[4];
+                    ReadProcessMemory(processHandle, currentChargeAddress, chargeBuffer, chargeBuffer.Length, out _);
+                    isCharging = BitConverter.ToInt32(chargeBuffer, 0);
                 }
+
+                Dispatcher.Invoke(() =>
+                {
+                    if (batteryLevel >= 0 && batteryLevel <= 100)
+                    {
+                        BatteryText.Text = $"{batteryLevel}%";
+                        BatteryFill.Width = (batteryLevel / 100.0) * 46.0;
+                    }
+
+                    if (isCharging == 1)
+                    {
+                        ChargingIcon.Visibility = Visibility.Visible;
+                    }
+                    else
+                    {
+                        ChargingIcon.Visibility = Visibility.Hidden;
+                    }
+                });
             }
             catch (Exception ex)
             {
